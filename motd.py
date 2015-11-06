7d1de99b9998aea2e84a035419f842e554fb826e
Mino <mino@minomino.org>
2015-11-06 19:42:10 +0100

    Added commands to manipulate all MOTDs at once.

--- motd.py
+++ motd.py
@@ -28,9 +28,12 @@ class motd(minqlx.Plugin):
         super().__init__()
         self.add_hook("player_loaded", self.handle_player_loaded, priority=minqlx.PRI_LOWEST)
         self.add_command(("setmotd", "newmotd"), self.cmd_setmotd, 4, usage="<motd>")
+        self.add_command(("setmotdall", "newmotdall"), self.cmd_setmotdall, 4, usage="<motd>")
         self.add_command(("getmotd", "motd"), self.cmd_getmotd)
         self.add_command(("clearmotd", "removemotd", "remmmotd"), self.cmd_clearmotd, 4)
+        self.add_command(("clearmotdall", "removemotdall", "remmmotdall"), self.cmd_clearmotdall, 4)
         self.add_command("addmotd", self.cmd_addmotd, 4, usage="<more_motd>")
+        self.add_command("addmotdall", self.cmd_addmotdall, 4, usage="<more_motd>")
 
         # homepath doesn't change runtime, so we can just save it for the sake of efficiency.
         self.home = self.get_cvar("fs_homepath")
@@ -61,6 +64,16 @@ class motd(minqlx.Plugin):
         self.db[self.motd_key] = " ".join(msg[1:])
         player.tell("The MOTD has been set.")
         return minqlx.RET_STOP_EVENT
+
+    def cmd_setmotdall(self, player, msg, channel):
+        motds = self.db.smembers(MOTD_SET_KEY)
+        db = self.db.pipeline()
+        for path in motds:
+            motd_key = MOTD_SET_KEY + ":{}".format(path)
+            db.set(motd_key, " ".join(msg[1:]))
+        db.execute()
+        player.tell("All MOTDs have been set.")
+        return minqlx.RET_STOP_EVENT
     
     def cmd_getmotd(self, player, msg, channel):
         motd = self.db[self.motd_key]
@@ -75,6 +88,12 @@ class motd(minqlx.Plugin):
         player.tell("The MOTD has been cleared.")
         return minqlx.RET_STOP_EVENT
 
+    def cmd_clearmotdall(self, player, msg, channel):
+        motds = [MOTD_SET_KEY + ":{}".format(m) for m in self.db.smembers(MOTD_SET_KEY)]
+        self.db.delete(*motds)
+        player.tell("All MOTDs have been cleared.")
+        return minqlx.RET_STOP_EVENT
+
     def cmd_addmotd(self, player, msg, channel):
         motd = self.db[self.motd_key]
         if not motd:
@@ -85,6 +104,19 @@ class motd(minqlx.Plugin):
             self.db[self.motd_key] = motd + leading_space + " ".join(msg[1:])
             player.tell("The MOTD has been updated.")
 
+        return minqlx.RET_STOP_EVENT
+
+    def cmd_addmotdall(self, player, msg, channel):
+        motds = self.db.smembers(MOTD_SET_KEY)
+        for path in motds:
+            motd_key = MOTD_SET_KEY + ":{}".format(path)
+            if motd_key not in self.db:
+                self.db[motd_key] = " ".join(msg[1:])
+            else:
+                motd = self.db[motd_key]
+                leading_space = "" if len(motd) > 2 and motd[-2:] == "\\n" else " "
+                self.db[motd_key] = motd + leading_space + " ".join(msg[1:])
+        player.tell("Added to all MOTDs.")
         return minqlx.RET_STOP_EVENT
 
     def send_motd(self, player, motd):
