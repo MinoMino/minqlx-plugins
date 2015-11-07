1345eebfe3309ec644787d1abcee9d86fdf24657
Mino <mino@minomino.org>
2015-11-07 12:04:40 +0100

    Do not require the Steam name to be the same for persistance if qlx_enforceSteamName is off.

--- names.py
+++ names.py
@@ -33,7 +33,7 @@ class names(minqlx.Plugin):
         name_key = _name_key.format(player.steam_id)
         if name_key in self.db:
             db_name = self.db[name_key]
-            if self.clean_text(db_name) == player.clean_name:
+            if not self.get_cvar("qlx_enforceSteamName", bool) or self.clean_text(db_name) == player.clean_name:
                 info = minqlx.parse_variables(minqlx.player_info(player.id)["userinfo"], ordered=True)
                 info["name"] = db_name
                 new_info = "".join(["\\{}\\{}".format(key, info[key]) for key in info])
