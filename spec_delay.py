1a33f21d3ac8362e940362e04cd6d974a27eb4b8
Christopher Stewart <stewpot64@gmail.com>
2015-11-13 13:41:43 +0000

    Added comments to spec_delay.

--- spec_delay.py
+++ spec_delay.py
@@ -13,16 +13,18 @@ class spec_delay(minqlx.Plugin):
         self.spec_delays.pop(player.steam_id, None)
 
     def handle_team_switch(self, player, old_team, new_team):
-        # This is only needed to stop \team s; team f
+        """Sets a delay on joining when the player joins spectator"""
         if new_team == "spectator" and old_team == "free":
             self.spec_delays[player.steam_id] = True
             self.allow_join(player)
+        # This is only needed to stop \team s; team f
         elif new_team == "free" and old_team == "spectator":
             if self.spec_delays.get(player.steam_id):
-                player.tell("^6You must wait  seconds before joining after speccing")
+                player.tell("^6You must wait 8 seconds before joining after speccing")
                 return minqlx.RET_STOP_EVENT
 
     def handle_team_switch_attempt(self, player, old_team, new_team):
+        """Stops the player joining if spec delay is true."""
         if new_team == "any" and old_team == "spectator":
             if self.spec_delays.get(player.steam_id):
                 player.tell("^6You must wait 8 seconds before joining after speccing")
@@ -30,6 +32,7 @@ class spec_delay(minqlx.Plugin):
 
     @minqlx.delay(8)
     def allow_join(self, player):
-        if self.spec_delays.get(player.steam_id) is not None:
+        """Allows the player to join after 8 seconds."""
+        if self.spec_delays.get(player.steam_id):
             player.tell("^6You can join now")
             self.spec_delays[player.steam_id] = False
