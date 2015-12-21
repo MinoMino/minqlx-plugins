d262df983e778de49aa0e4e62690fd1d2c5b00c7
Christopher Stewart <cstewart90@users.noreply.github.com>
2015-12-21 15:43:53 +0000

    Set spec delay on disconnect and delete key instead of setting to False.

--- spec_delay.py
+++ spec_delay.py
@@ -21,8 +21,9 @@ class spec_delay(minqlx.Plugin):
         self.spec_delays = {}
 
     def handle_player_disconnect(self, player, reason):
-        """Remove player key when they disconnect."""
-        self.spec_delays.pop(player.steam_id, None)
+        """Also set spec delay when a player disconnects."""
+        self.spec_delays[player.steam_id] = True
+        self.allow_join(player)
 
     def handle_team_switch(self, player, old_team, new_team):
         """Sets a delay on joining when the player joins spectator"""
@@ -38,15 +39,17 @@ class spec_delay(minqlx.Plugin):
 
     def handle_team_switch_attempt(self, player, old_team, new_team):
         """Stops the player joining if spec delay is true."""
-        if (new_team == "any" or new_team == "free") and old_team == "spectator":
+        if new_team != "spectator" and old_team == "spectator":
             if self.spec_delays.get(player.steam_id):
                 player.tell("^6You must wait 8 seconds before joining after spectating")
                 return minqlx.RET_STOP_EVENT
 
     @minqlx.delay(8)
     def allow_join(self, player):
-        """Allows the player to join after 8 seconds."""
+        """Allows the player to join after 8 seconds.
+        :param player: The player to allow join for.
+        """
         if self.spec_delays.get(player.steam_id):
             # Remove spec delay
-            self.spec_delays[player.steam_id] = False
+            self.spec_delays.pop(player.steam_id, None)
             player.tell("^6You can join now")
