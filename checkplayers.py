25bf7d6e175b0e9e9c3b474a3f44e4d8d22097c0
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-05-17 11:53:53 +0100

    Made tell_large_output a function. Made other commands use tell_large_output.

--- checkplayers.py
+++ checkplayers.py
@@ -12,7 +12,7 @@ TODO: Check if there is no banned/silenced etc players instead of returing empty
 
 import minqlx
 import minqlx.database
-import operator
+from operator import itemgetter
 import re
 import time
 
@@ -37,39 +37,39 @@ class checkplayers(minqlx.Plugin):
             steam_id = key.split(":")[2]
             permission = self.db[key]
             name = self.player_name(steam_id)
-
             players.append({"name": name, "steam_id": steam_id, "permission": permission})
-        players.sort(key=operator.itemgetter("permission"), reverse=True)
 
-        output = "Owner: {} Name: {}\n".format(minqlx.owner(), self.player_name(minqlx.owner()))
-        output += "{:^24} | {:^17} | {}\n".format("Name", "Steam ID", "Permission")
-        for p in players:
-            output += "{name:24} | {steam_id:17} | {permission}\n".format(**p)
-        player.tell(output)
+        output = ["^5Owner: ^2{} Name: ^7{}".format(minqlx.owner(), self.player_name(minqlx.owner())),
+                  "{:^24} | {:^17} | {}\n".format("Name", "Steam ID", "Permission")]
+        for p in sorted(players, key=itemgetter("permission"), reverse=True):
+            output.append("{name:24} | {steam_id:17} | {permission}\n".format(**p))
+        tell_large_output(player, output)
 
     @minqlx.thread
     def cmd_silenced(self, player, msg, channel):
-        output = "{:^24} | {:^17} | {:^19} | {}".format("Name", "Steam ID", "Expires", "Reason")
+        output = ["{:^24} | {:^17} | {:^19} | {}".format("Name", "Steam ID", "Expires", "Reason")]
+
         for key in self.db.scan_iter("minqlx:players:*:silences"):
             steam_id = key.split(":")[2]
             silenced = self.plugins["silence"].is_silenced(steam_id)
             if silenced:
                 expires, score, reason = silenced
                 name = self.player_name(steam_id)
-                output += "{:24} | {:17} | {} | {}\n".format(name, steam_id, expires, reason)
-        self.tell_large_output()
+                output.append("{:24} | {:17} | {} | {}".format(name, steam_id, expires, reason))
+        tell_large_output(player, output)
 
     @minqlx.thread
     def cmd_banned(self, player, msg, channel):
-        output = "{:^24} | {:^17} | {:^19} | {}".format("Name", "Steam ID", "Expires", "Reason")
+        output = ["{:^24} | {:^17} | {:^19} | {}".format("Name", "Steam ID", "Expires", "Reason")]
+
         for key in self.db.scan_iter("minqlx:players:*:bans"):
             steam_id = key.split(":")[2]
             banned = self.plugins["ban"].is_banned(steam_id)
             if banned:
                 expires, reason = banned
                 name = self.player_name(steam_id)
-                output += "{:24} | {:17} | {} | {}\n".format(name, steam_id, expires, reason)
-        player.tell(output)
+                output.append("{:24} | {:17} | {} | {}\n".format(name, steam_id, expires, reason))
+        tell_large_output(player, output)
 
     @minqlx.thread
     def cmd_leaver_banned(self, player, msg, channel):
@@ -77,7 +77,7 @@ class checkplayers(minqlx.Plugin):
             player.tell("Leaver ban is not enabled.")
         else:
             output = self.leavers("ban")
-            self.tell_large_output(player, output)
+            tell_large_output(player, output)
 
     @minqlx.thread
     def cmd_leaver_warned(self, player, msg, channel):
@@ -89,6 +89,7 @@ class checkplayers(minqlx.Plugin):
     def leavers(self, action):
         output = ["^5{:^24} | {:^17} | {} | {} | {}"
                   .format("Name", "Steam ID", "Left", "Completed", "Ratio")]
+
         for key in self.db.scan_iter("minqlx:players:*:games_left"):
             steam_id = key.split(":")[2]
             status = self.plugins["ban"].leave_status(steam_id)
@@ -115,10 +116,11 @@ class checkplayers(minqlx.Plugin):
             name = steam_id
         return name
 
-    def tell_large_output(self, player, output):
-        """Tells large output in small portions, as not to disconnected the player."""
-        for count, line in enumerate(output, start=1):
-            if count % 30 == 0:  # change 30 to a smaller value, if someone getting disconnected
-                time.sleep(0.4)  # change 0.4 to a bigger value, if changing previous line does not help
-                player.tell(output[0])
-            player.tell(line)
+
+def tell_large_output(player, output):
+    """Tells large output in small portions, as not to disconnected the player."""
+    for count, line in enumerate(output, start=1):
+        if count % 30 == 0:  # decrease if someone getting disconnected
+            time.sleep(0.4)  # increase if changing previous line does not help
+            player.tell(output[0])
+        player.tell(line)
