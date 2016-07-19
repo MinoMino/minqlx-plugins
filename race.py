d77b3fc91904bd1050be757c0ee593acfad04a9c
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-07-19 23:33:44 +0100

    Fixed wrong settings for pgultimate

--- race.py
+++ race.py
@@ -31,9 +31,10 @@ GAUNTLET_AND_MG = ("blockworld", "caep4", "climbworld", "df_etleague", "df_extre
                    "purpletorture", "r7_pyramid", "raveroll", "sl1k_tetris_easy", "snorjumpb1", "sodomia", "timelock2",
                    "timelock4", "vanilla_03", "vanilla_04", "vanilla_07", "vanilla_10", "walkathon", "weirdwild",
                    "wraiths", "yellowtorture", "run139", "inder_inder", "quartz", "timelock3")
-PLASMA = ("think1", "xproject", "plasmax", "wub_junk")
+
+PLASMA = ("think1", "xproject", "plasmax", "wub_junk", "pgultimate")
 ROCKET = ("runstolfer", "charon", "charon_bw", "kozmini1", "kozmini2", "kozmini3", "kozmini4", "kozmini5", "kozmini6",
-          "kozmini7", "kozmini8", "jumpspace", "pgultimate")
+          "kozmini7", "kozmini8", "jumpspace")
 GRENADE = ("grenadorade")
 
 _RE_POWERUPS = re.compile(r'print ".+\^3 got the (Haste|Battle Suit|Quad Damage)!\^7\n"')
