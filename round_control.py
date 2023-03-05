5512ba0c2d70f135a6fd1385dfcc638762dcaa75
Victor Gianotto <Gianotto@users.noreply.github.com>
2023-03-05 17:08:17 -0300

    Update round_control.py

--- round_control.py
+++ round_control.py
@@ -17,7 +17,7 @@
 # along with minqlx. If not, see <http://www.gnu.org/licenses/>.
 
 # Script blocks new players to join the game after ## rounds has been passed.
-# 
+# Allow players to callvote unlock to let spectators to join.
 
 import minqlx
 
@@ -33,13 +33,13 @@ class round_control(minqlx.Plugin):
         self.add_hook("new_game", self.handle_new_game)
         self.add_hook("vote_ended", self.handle_vote_ended)
 
-        self.add_command(("unlock"), self.cmd_unlockteams, 5)
-        self.add_command(("lock"), self.cmd_lockteams, 5)
-        
+        self.add_command(("unlockteams"), self.cmd_unlockteams, 5)
+        self.add_command(("lockteams"), self.cmd_lockteams, 5)
+
         self.set_cvar_once("qlx_minRoundsToLock", "5") # minimum rounds to block new players to join
 
     def handle_vote_ended(self, votes, vote, args, passed):   
-        if passed == True and vote.lower() == "unlock":
+        if passed == True and vote.lower() == "unlockteams":
             gt = self.game.type_short
             if gt not in SUPPORTED_GAMETYPES:
                 return
@@ -48,14 +48,27 @@ class round_control(minqlx.Plugin):
             def f():
                 if LOCKED == True:
                     LOCKED = False
+                    self.unlock()
                     self.msg("Teams were ^6UNLOCKED^7. Spectators are allowed to join.")
                     return
-                
             f()
 
     def cmd_lockteams(self, player, msg, channel):
-
-        return
-    def cmd_unlockteams(self, player, msg, channel):
+        teams = self.teams()
+        players = teams["red"] + teams["blue"]
+        if players % 2 != 0:
+            self.msg("Teams were ^6NOT^7 balanced due to the total number of players being an odd number.")
+            return
         
-        return
+        self.game.teamsize = teams["red"]
+        self.lock()
+        LOCKED = True
+        self.msg("Teams has been ^6LOCKED^7.")
+    
+    def cmd_unlockteams(self, player, msg, channel):
+        teams = self.teams()
+        players = teams["red"]
+        self.game.teamsize = teams["red"] + 1
+        LOCKED = False
+        self.unlock()
+        self.msg("Teams were ^6UNLOCKED^7. Spectators are allowed to join.")
